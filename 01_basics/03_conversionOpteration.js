73fc13f9cd6ba654227c6ea64a49240ba82e3540
Anmol Bharadwaj <42213025+AnmolBharadwaj@users.noreply.github.com>
2024-03-13 03:31:28 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOpteration.js
@@ -0,0 +1,29 @@
+let score = true
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "69" => 69
+// "69abc" => Nan
+// true => 1; false => 0
+// undefined => 
+
+let loggedIn = "Anmol"
+
+let BooleanIsLoggedIn = Boolean(loggedIn)
+// console.log(BooleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Anmol" => true
+
+let someNumber = 69
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
