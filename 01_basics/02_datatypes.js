b00a8a7fba4b2d97c1a47fe6de879379d93289ce
Anmol Bharadwaj <42213025+AnmolBharadwaj@users.noreply.github.com>
2024-03-13 03:00:13 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // content readabilty should be high
+
+console.log("Anmol");
+
+let name = "Anmol" // string datatype
+let age = 22 // number datatype
+let isLoggedIn = true //Boolean datatype
+let state; // undefined datatype
+
+// number => 2 to the power of 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
