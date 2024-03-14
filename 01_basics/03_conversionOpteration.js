1e622ef8058c8a94ca73188fac0b44ab0232894b
Anmol Bharadwaj <42213025+AnmolBharadwaj@users.noreply.github.com>
2024-03-14 07:36:05 +0000

    Operations

--- 01_basics/03_conversionOpteration.js
+++ 01_basics/03_conversionOpteration.js
@@ -25,5 +25,52 @@ let BooleanIsLoggedIn = Boolean(loggedIn)
 let someNumber = 69
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ***************** Operations *******************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2); // Addition operation
+// console.log(2-2); // subtraction 
+// console.log(2*2); // Multiplication
+// console.log(2**3); // Power
+// console.log(2/2); //division
+// console.log(2%3); // Remainder
+
+let str1 = "Hello"
+let str2 = " Anmol"
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("2" + 3);
+// console.log(2 + "3");
+// console.log("2" + 3 + 3);  // preferred type
+// console.log(2 + 3 + "3"); // preferred type
+
+// console.log(((2 + 3) * 5) % 4);
+
+// console.log(true);
+// console.log(+true);
+// console.log("");
+// console.log(+"");
+
+let x = 3
+const y = x++
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+// If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing.
+
+// If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.
