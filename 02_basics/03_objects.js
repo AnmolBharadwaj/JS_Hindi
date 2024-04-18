1fa1ba7f7db9f1e2d6cc269200791721d87e86a5
Anmol Bharadwaj <42213025+AnmolBharadwaj@users.noreply.github.com>
2024-04-18 08:40:24 +0000

    object

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,34 @@
+// Object.create // Creating object using constructor
+
+// object literals
+
+const mySym = Symbol("key1") // symbol
+
+const JsUser = {
+    name: "Anmol", // name is key and Anmol is a value
+    "full name": "Anmol Bharadwaj",
+    [mySym]: "myKey1", // Correct syntax to define a symbol and use it as a key
+    age: 22,
+    Location: "Noida",
+    Mail: "Anmol@google.com",
+    IsLoggedIn: false,
+    LastlogInDays: ["Monday", "Wednesday", "Friday"]
+}
+
+console.log(JsUser);
+console.log(JsUser.Mail);
+console.log(JsUser["Mail"]);
+console.log(JsUser["full name"]);
+console.log(JsUser[mySym]);
+
+JsUser.Mail = "Anmol@yahoo.com"
+// Object.freeze(JsUser) // used to freeze an object so no changes could be made in that object after that
+JsUser.Mail = "Anmol@riot.com"
+
+console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS family");
+}
+
+console.log(JsUser.greeting());
